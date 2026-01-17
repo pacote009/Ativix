d9ea28f3da35958dfa8b37fe19cdba9b4bce9c79
Vitor Mello <vitor_melloleite@hotmail.com>
2026-01-17 13:04:12 -0300

    Aumenta limite upload

--- os-backend/src/index.js
+++ os-backend/src/index.js
@@ -12,13 +12,15 @@ import dashboardRoutes from './routes/dashboard.js';
 import relatoriosRoutes from './routes/relatorios.js';
 import tonersRoutes from './routes/toners.js';
 
-
-
-
-
 const app = express();
+
 app.use(cors());
-app.use(express.json());
+
+// --- ALTERAÇÃO AQUI ---
+// Aumentamos o limite para 50mb para aceitar imagens em Base64
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+// ----------------------
 
 // rotas
 app.use('/auth', authRoutes);
